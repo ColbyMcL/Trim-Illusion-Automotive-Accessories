8fad4f97a4afcc8a94888c4c0780467c0478c1e2
anil <anil-pattel136>
2022-02-22 13:19:18 +0530

    website stock module added

---
+++ website_product_stock/models/product.py
@@ -0,0 +1,28 @@
+
+from odoo import models, fields, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    def product_qty_get(self):
+        warehouse_line = []
+        for warehouse_id in self.env['stock.warehouse'].search([]):
+            ctx = {'warehouse': warehouse_id.id, 'product_id': self}
+            stock = self.with_context(ctx)._compute_quantities_dict(
+                lot_id=False, owner_id=False, package_id=False,
+                from_date=False, to_date=False)
+            if stock:
+                warehouse_line.append({
+                    'warehouse_id': warehouse_id.id,
+                    'warehouse_name': warehouse_id.name,
+                    'qty': stock.get(self.id).get(
+                        'qty_available'),
+                    'incoming_qty': stock.get(self.id).get(
+                        'incoming_qty'),
+                    'outgoing_qty': stock.get(self.id).get(
+                        'outgoing_qty'),
+                    'forecasted_qty': stock.get(self.id).get(
+                        'virtual_available')
+                })
+        return warehouse_line
